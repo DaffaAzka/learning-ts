99ab2f4ee015695deb60dd4ebcf36f89b9b00dbc
Daffa Azka <azkadaiki26@gmail.com>
2025-04-27 21:20:32 +0700

    intro

---
+++ variableMe.js
@@ -0,0 +1,2 @@
+var greetings = "Hello, Dest!";
+console.log(greetings);
---
+++ variableMe.ts
@@ -0,0 +1,10 @@
+let greetings: string = "Hello, Dest!";
+// greetings = 6;
+
+let num: number = 26;
+
+// num.toUpperCase();
+
+console.log(greetings);
+
+export {};

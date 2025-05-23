35341d984c8b3f8938debfa38d928786f458e2fa
Daffa Azka <azkadaiki26@gmail.com>
2025-05-23 23:06:43 +0700

    function part 2

--- myFunction.js
+++ myFunction.js
@@ -12,6 +12,29 @@ function signUp(username, email, isPaid) {
 var loginUser = function (email, password, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
+// function getValue(myValue:number): boolean {
+//     if (myValue > 5) {
+//         return true;
+//     }
+//     return false;
+// }
+var getHello = function (s) {
+    return "hello ".concat(s);
+};
+var heros = [
+    "Superman",
+    "Batman",
+    "Wonder Woman"
+];
+heros.map(function (hero) {
+    return "Hero is ".concat(hero);
+});
+function consoleError(msg) {
+    console.log(msg);
+}
+function handleError(err) {
+    throw new Error(err);
+}
 addTwo(5);
 getUpper("hello");
 signUp("Daffa Azka", "azka@gmail.com", true);
--- myFunction.ts
+++ myFunction.ts
@@ -1,11 +1,11 @@
 
-function addTwo(n: number) {
+function addTwo(n: number): number {
     // n.toUpperCase(); // Error: Property 'toUpperCase' does not exist on type 'number'. 
 
     return n += 2;
 }
 
-function getUpper(s: string) {
+function getUpper(s: string): string {
     return s.toUpperCase();
 }
 
@@ -15,6 +15,36 @@ function signUp(username: String, email: String, isPaid: boolean) {
 
 let loginUser = (email: string, password: string, isPaid: boolean = false) => {
 
+}
+
+// function getValue(myValue:number): boolean {
+//     if (myValue > 5) {
+//         return true;
+//     }
+
+//     return false;
+// }
+
+const getHello = (s: string): string => {
+    return `hello ${s}`;
+}
+
+const heros = [
+    "Superman",
+    "Batman",
+    "Wonder Woman"
+]
+
+heros.map((hero: string): string => {
+    return `Hero is ${hero}`;
+})
+
+function consoleError(msg:string): void {
+    console.log(msg);
+}
+
+function handleError(err: string): never {
+    throw new Error(err);
 }
 
 addTwo(5);

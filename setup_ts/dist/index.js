411276d048cadc9688c58073a752d187f285b727
Daffa Azka <azkadaiki26@gmail.com>
2025-05-28 23:57:38 +0700

    setup real ts project

---
+++ setup_ts/dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+console.log('This is the entry point of the TypeScript setup.');
+// export {}
---
+++ setup_ts/src/index.ts
@@ -0,0 +1,4 @@
+
+console.log('This is the entry point of the TypeScript setup.');
+
+// export {}

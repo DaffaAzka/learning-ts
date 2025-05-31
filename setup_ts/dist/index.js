e4658ad3799201f42a85285a930bbbc92cd84566
Daffa Azka <azkadaiki26@gmail.com>
2025-05-31 21:19:43 +0700

    classes

--- setup_ts/dist/index.js
+++ setup_ts/dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
 console.log('This is the entry point of the TypeScript setup.');
+class User {
+    constructor(email, name) {
+        this.city = "";
+        this.email = email,
+            this.name = name;
+    }
+}
+const dest = new User("azkadaiki@gmail.com", "Dest");
+dest.city = "Jakarta";
 // export {}
--- setup_ts/src/index.ts
+++ setup_ts/src/index.ts
@@ -1,4 +1,22 @@
 
 console.log('This is the entry point of the TypeScript setup.');
 
+class User {
+
+    // let email, name;
+
+    email: string;
+    name: string;
+    city: string = "";
+
+    constructor(email: string, name: string) {
+        this.email = email,
+        this.name = name
+    }
+
+}
+
+const dest = new User("azkadaiki@gmail.com", "Dest")
+dest.city = "Jakarta"
+
 // export {}
